26b106215f05d73b7ebbb6895be17cefef29d8c6
Halldor Rolandsson <etwas@black-flirt.de>
2016-07-24 00:27:26 +0200

    Collections begonnen

--- gallery.php
+++ gallery.php
@@ -49,47 +49,116 @@
      */
     if(isset($_GET['gallery'])) {
         
-        $offset = request_var('offset', 0);
+        $mode = request_var('gallery', 'overview');
         
-        $sql = 'SELECT id, name FROM phpbb_gallery ORDER BY id DESC LIMIT 40 OFFSET ' . $offset;
-        $result = $db->sql_query($sql);
-        
-        while($row = $db->sql_fetchrow($result)) {
-            $data[$row['id']]['id'] = $row['id'];
-            $data[$row['id']]['name'] = $row['name'];
+        if($mode == 'images') {
+            $offset = request_var('offset', 0);
+
+            $sql = 'SELECT id, name FROM phpbb_gallery ORDER BY id DESC LIMIT 40 OFFSET ' . $offset;
+            $result = $db->sql_query($sql);
+
+            while($row = $db->sql_fetchrow($result)) {
+                $data[$row['id']]['id'] = $row['id'];
+                $data[$row['id']]['name'] = $row['name'];
+            }
+
+            foreach($data as $pic) {
+
+               $template->assign_block_vars('pic', array(
+                    'NAME'      => $pic['name'],
+                    'PICTURE'   => $pic['id'],
+                )); 
+            }
+
+            $sql = 'SELECT count(*) AS count FROM phpbb_gallery';
+            $result = $db->sql_query($sql);
+            $row = $db->sql_fetchrow($result);
+
+            $max_files = $row['count']; 
+            $curSite = ($offset + 1) / 40;
+
+            $template->assign_vars(array(
+                'PREVIOUS'  => $offset-40,
+                'NEXT'      => $offset+40,
+                'CANADD'    => $canAddPictures,
+                'MAXSITES'  => ceil($max_files / 40),
+                'ROWS'      => $max_files,
+                'SITE'      => floor($curSite) + 1,
+            ));
+
+            // Inits Languagefile
+            page_header('Galerie');
+
+            // Load Template
+            $template->set_filenames(array(
+                'body' => 'showGallery.html',
+            ));
         }
         
-        foreach($data as $pic) {
+        if($mode == 'overview') {
+            // Folder auslesen
+            $sql = 'SELECT id, foldername FROM phpbb_gallery_folders ORDER BY id DESC LIMIT 8';
+            $result = $db->sql_query($sql);
+            
+            while($row = $db->sql_fetchrow($result)) {
+                $data[$row['id']]['id'] = $row['id'];
+                $data[$row['id']]['name'] = $row['foldername'];
+            }
+            
+            // Thumbs für Folder holen
+            foreach($data as $folder) {
+                
+                $template->assign_block_vars('folder', array(
+                    'ID'    => $folder['id'],
+                    'NAME'  => $folder['name'],
+                ));
+                
+                $sql = 'SELECT id FROM phpbb_gallery WHERE in_group= '. $folder['id'] .' LIMIT 9';
+                $result = $db->sql_query($sql);
+                
+                while($row = $db->sql_fetchrow($result)) {
+                    $template->assign_block_vars('folder.thumbs', array(
+                        'ID'    => $row['id'],
+                ));
+                }
+            }
+            
+            $tmp = array(
+                'SELECT count(*) AS count FROM phpbb_gallery',
+                'SELECT count(*) AS count FROM phpbb_gallery_folders'
+            );
+            
+            foreach($tmp as $sql) {
+                $result = $db->sql_query($sql);
+                $row = $db->sql_fetchrow($result);
+                $count[] .= $row['count'];
+                
+                
+            }
+            
+            $template->assign_vars(array(
+                'MAXCOLL' => $count[1] - 8,
+                'MAXFILE'   => $count[0] - 8,
+            ));
+            
+            // Bilder auslesen
+            $sql = 'SELECT id, name FROM phpbb_gallery ORDER BY id DESC LIMIT 8';
+            $result = $db->sql_query($sql);
+            
+            while($row = $db->sql_fetchrow($result)) {
+                $template->assign_block_vars('pic', array(
+                    'NAME'      => $row['name'],
+                    'ID'        => $row['id'],
+                ));
+            }
 
-           $template->assign_block_vars('pic', array(
-                'NAME'      => $pic['name'],
-                'PICTURE'   => $pic['id'],
-            )); 
+            page_header('Usergalerie');
+
+            // Load Template
+            $template->set_filenames(array(
+                'body' => 'galleryOverview.html',
+            ));
         }
-        
-        $sql = 'SELECT count(*) AS count FROM phpbb_gallery';
-        $result = $db->sql_query($sql);
-        $row = $db->sql_fetchrow($result);
-        
-        $max_files = $row['count']; 
-        $curSite = ($offset + 1) / 40;
-        
-        $template->assign_vars(array(
-            'PREVIOUS'  => $offset-40,
-            'NEXT'      => $offset+40,
-            'CANADD'    => $canAddPictures,
-            'MAXSITES'  => ceil($max_files / 40),
-            'ROWS'      => $max_files,
-            'SITE'      => floor($curSite) + 1,
-        ));
-        
-        // Inits Languagefile
-        page_header('Galerie');
-
-        // Load Template
-        $template->set_filenames(array(
-            'body' => 'showGallery.html',
-        ));
     }
     
     /**

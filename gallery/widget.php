7cbfc621c933b2d83893f13258c4be22b791c9e2
Halldor Rolandsson <etwas@black-flirt.de>
2016-07-23 12:33:33 +0200

    Widget fertig

---
+++ gallery/widget.php
@@ -0,0 +1,68 @@
+<?php 
+    define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include('../common.php');
+require_once('php_image_magician.php');
+
+$sql = "SELECT id FROM phpbb_gallery ORDER BY id DESC LIMIT 1";
+$result = $db->sql_query($sql);
+$row = $db->sql_fetchrow($result);
+
+$maxId = $row['id'];
+
+$sql = "SELECT id FROM phpbb_gallery ORDER BY id ASC LIMIT 1";
+$result = $db->sql_query($sql);
+$row = $db->sql_fetchrow($result);
+
+$minId = $row['id'];
+
+$random = mt_rand($minId, $maxId);
+
+$pictureId = false;
+while($pictureId == false) {
+    $sql = "SELECT id, name FROM phpbb_gallery WHERE id = " . $random;
+    $result = $db->sql_query($sql);
+    $row = $db->sql_fetchrow($result);
+    
+    $pictureId = $row['id'];
+    $pictureName = $row['name'];
+}
+
+if(strlen($pictureName) >= 20) {
+    $pictureName = substr($pictureName, 0, 15) . '..';
+}
+
+$path = 'C:\xampp\htdocs\jgzga\images\gallery\\';
+
+$im = new imageLib($path . $pictureId . '.jpg');
+$height = $im->getOriginalHeight();
+$width  = $im->getOriginalWidth();
+
+if($height > $width) {
+    $class = 'portrait';
+} else {
+    $class = 'landscape';
+}
+
+?>
+
+<html>
+    <head>
+            <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <title>Zufallsbild</title>
+    </head>
+    <body>
+
+    <div class="widget-randompicture">
+        <div>
+            <img class="<?php echo $class; ?>" src="../images/gallery/<?php echo $pictureId; ?>.jpg">
+        </div>
+        <div>
+            <p><?php echo $pictureName; ?></p>
+        </div>
+    </div>
+
+    </body>
+</html>

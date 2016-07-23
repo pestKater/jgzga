9a77d9dfadef73483eae6f9dacbc8479ae55c955
Browser3 <j.steckelberg@web.de>
2016-07-23 15:54:17 +0200

    Kommandostand New Function
    Noch Buggy..

--- kommandostand/index.php
+++ kommandostand/index.php
@@ -1,9 +1,21 @@
 <?php
 require_once 'functions.php';
 
-$aktueller_server = 1;
+$aktueller_server = 2;
 $aktueller_modstring = 1;
 $admin_wert = 0;
+
+/*
+$name = '#1 Public';
+$port = '2312';
+$server_dir = 'C:\working\server\a3\s01';
+$exe = 'a3s01.exe';
+$gametype = 'A3';
+$basic_cfg = 'basic.cfg';
+$server_cfg = 'server.cfg';
+$admin = '1';
+$params = '-server -noLogs -enableHT -maxmem=2048 -high "-malloc=tbbmalloc" -profiles=C:\ArmA3\A3Master -loadMissionToMemory';
+*/
 
 //$serverParams = generateParams($aktueller_server);
 //$modstrings = generateModstring($aktueller_modstring);
@@ -11,6 +23,7 @@ $admin_wert = 0;
 
 //$server_array = getServerInfos($aktueller_server);
 //$modstring_array = getModstringInfos($aktueller_modstring);
+//sentServerInfos($aktueller_server, $name, $port, $server_dir, $exe, $gametype, $basic_cfg, $server_cfg, $admin, $params);   // Geht noch nicht
 
 //changeServerlock($aktueller_server,$admin_wert);
 

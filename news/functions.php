c35c80eab3db0f4e80d409a419f5cc2b7eb48539
Halldor Rolandsson <etwas@black-flirt.de>
2016-08-09 10:54:16 +0200

    resetted

--- news/functions.php
+++ news/functions.php
@@ -86,9 +86,8 @@ function getOverviewArticles($isMember) {
     } else {
         $sql = "SELECT id, title, content, category FROM phpbb_news_articles WHERE " . $db->sql_in_set('eventCategory', $intern, true) . " OR eventCategory IS NULL ORDER BY id DESC LIMIT 5 OFFSET 1";
     }
+    
     $result = $db->sql_query($sql);
-    var_dump($result);
-    die;
     while($row = $db->sql_fetchrow($result)){
         $data[$row['id']]['id'] = $row['id'];
         $data[$row['id']]['title'] = $row['title'];

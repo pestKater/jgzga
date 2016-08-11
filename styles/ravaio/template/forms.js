32d16f4600dada26ae088883496bc0fd23c08d9b
Halldor Rolandsson <etwas@black-flirt.de>
2016-08-11 15:24:27 +0200

    test

---
+++ styles/ravaio/template/forms.js
@@ -0,0 +1,27 @@
+$('form').submit( function(ev) {
+    var textarea = $('form textarea');
+    var text = textarea.val();
+
+    text = text.replace(/ä/g,"&auml;");
+    text = text.replace(/ö/g, '&ouml;');
+    text = text.replace(/ü/g, '&uuml;');
+    text = text.replace(/Ä/g, '&Auml;');
+    text = text.replace(/Ö/g, '&Ouml;');
+    text = text.replace(/Ü/g, '&Uuml;');
+    text = text.replace(/ß/g, '&szlig;');
+
+    textarea.val(text);
+
+    var title = $('#title');
+    var text = title.val();
+
+    text = text.replace(/ä/g,"&auml;");
+    text = text.replace(/ö/g, '&ouml;');
+    text = text.replace(/ü/g, '&uuml;');
+    text = text.replace(/Ä/g, '&Auml;');
+    text = text.replace(/Ö/g, '&Ouml;');
+    text = text.replace(/Ü/g, '&Uuml;');
+    text = text.replace(/ß/g, '&szlig;');
+
+    title.val(text);
+});
